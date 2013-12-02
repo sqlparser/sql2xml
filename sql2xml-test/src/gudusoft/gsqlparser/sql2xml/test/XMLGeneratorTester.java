e5909d54cd7a4336ee337d37718d73dc260bf320
cchao <cchen@actuate.com>
2013-12-02 13:33:51 +0800

    Update the test code.

---
+++ sql2xml-test/src/gudusoft/gsqlparser/sql2xml/test/XMLGeneratorTester.java
@@ -0,0 +1,120 @@
+
+package gudusoft.gsqlparser.sql2xml.test;
+
+import gudusoft.gsqlparser.EDbVendor;
+import gudusoft.gsqlparser.sql2xml.generator.SQL2XMLGenerator;
+import gudusoft.gsqlparser.sql2xml.generator.SQL2XMLGeneratorFactory;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import junit.framework.TestCase;
+
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class XMLGeneratorTester extends TestCase
+{
+
+	public static class DTDEntityResolver implements EntityResolver
+	{
+
+		public InputSource resolveEntity( String publicId, String systemId )
+				throws SAXException, IOException
+		{
+			return new InputSource( ".\\dtd\\ansi2003.xsd" );
+		}
+	}
+
+	protected void setUp( ) throws Exception
+	{
+		File testBaseDir = new File( "./testcases" );
+		File[] testcases = testBaseDir.listFiles( );
+		SQL2XMLGenerator generator = SQL2XMLGeneratorFactory.getGenerator( EDbVendor.dbvansi );
+		for ( int i = 0; i < testcases.length; i++ )
+		{
+			File testcase = testcases[i];
+			String caseName = testcase.getName( );
+			if ( caseName.toLowerCase( ).endsWith( ".sql" ) )
+			{
+				String result = generator.generateXML( testcase );
+				if ( result == null )
+				{
+					System.err.println( generator.getErrorMessage( ) );
+				}
+				else
+				{
+					int index = caseName.lastIndexOf( '.' );
+					try
+					{
+						FileOutputStream fos = new FileOutputStream( new File( testBaseDir,
+								caseName.substring( 0, index ) + ".xml" ) );
+						OutputStreamWriter writer = new OutputStreamWriter( fos,
+								"UTF-8" );
+						writer.write( result );
+						writer.close( );
+					}
+					catch ( Exception e )
+					{
+						e.printStackTrace( );
+					}
+				}
+			}
+		}
+	}
+
+	public void testValidation( )
+	{
+		File testBaseDir = new File( "./testcases" );
+		File[] testcases = testBaseDir.listFiles( );
+		for ( int i = 0; i < testcases.length; i++ )
+		{
+			String caseName = testcases[i].getName( );
+			if ( caseName.toLowerCase( ).endsWith( ".xml" ) )
+			{
+				assertTrue( validateXMLFile( testcases[i] ) );
+			}
+		}
+	}
+
+	private boolean validateXMLFile( File testcase )
+	{
+		try
+		{
+			URL schemaFile = new File( ".\\dtd\\ansi2003.xsd" ).toURI( )
+					.toURL( );
+			Source xmlFile = new StreamSource( testcase );
+			SchemaFactory schemaFactory = SchemaFactory.newInstance( XMLConstants.W3C_XML_SCHEMA_NS_URI );
+			Schema schema = schemaFactory.newSchema( schemaFile );
+			Validator validator = schema.newValidator( );
+			try
+			{
+				validator.validate( xmlFile );
+				System.out.println( xmlFile.getSystemId( ) + " is valid" );
+				return true;
+			}
+			catch ( SAXException e )
+			{
+				System.out.println( xmlFile.getSystemId( ) + " is NOT valid" );
+				System.out.println( "Reason: " + e.getLocalizedMessage( ) );
+			}
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace( );
+		}
+
+		return false;
+	}
+}
--- sql2xml/src/gudusoft/gsqlparser/sql2xml/model/direct_sql_statement.java
+++ sql2xml/src/gudusoft/gsqlparser/sql2xml/model/direct_sql_statement.java
@@ -1,18 +1,21 @@
 
 package gudusoft.gsqlparser.sql2xml.model;
 
+import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.Element;
-import org.simpleframework.xml.Namespace;
-import org.simpleframework.xml.NamespaceList;
 import org.simpleframework.xml.Root;
 
 @Root
-@NamespaceList({
-		@Namespace(reference = "http://www.sqlparser.com/xml/ansi2003"),
-		@Namespace(reference = "http://www.w3.org/2001/XMLSchema-instance", prefix = "xsi"),
-})
 public class direct_sql_statement
 {
+	@Attribute
+	private String xmlns="http://www.sqlparser.com/xml/ansi2003";
+	
+	@Attribute(name = "xmlns:xsi")
+	private String namespace = "http://www.w3.org/2001/XMLSchema-instance";
+
+	@Attribute(name = "xsi:schemaLocation")
+	private String schemaLocation = "http://www.sqlparser.com/xml/ansi2003 http://sqlparser.com/xml/ansi2003/ansi2003.xsd";
 
 	@Element
 	private directly_executable_statement directly_executable_statement = new directly_executable_statement( );
